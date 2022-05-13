fe859c09d9d667205fd0e9e4f89a1832aebccee2
rodrigoarcanjo23 <rodrigoarcanjo690@gmail.com>
2022-05-12 23:11:43 -0300

    primeiro projeto typescript

---
+++ app/models/negociacoes.ts
@@ -0,0 +1,16 @@
+import { Negociacao } from "./negociacao";
+
+export class Negociacoes {
+    private negociacoes: Negociacao[] = [];
+
+   adiciona(negociacao: Negociacao) {
+       this.negociacoes.push(negociacao);
+
+   } 
+
+   lista(): readonly Negociacao[] {
+        return this.negociacoes;
+
+   }
+} 
+
